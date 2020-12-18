75ebeb47a2abcd763da74b97b82d8abd07e00390
Flashcyborg <kiranamyigulipalli@gmail.com>
2020-12-18 16:44:12 +0530

    modified

--- sketch.js
+++ sketch.js
@@ -9,9 +9,10 @@ var backgroundImg,platform;
 var bird, slingshot;
 
 var gameState = "onSling";
-
+var bg = "sprites/bg2.jpg";
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+  //backgroundImg = loadImage("sprites/bg.png");
+  getBackgroundImg ()
 }
 
 function setup(){
@@ -45,6 +46,9 @@ function setup(){
 }
 
 function draw(){
+    if (backgroundImg){
+
+    
     background(backgroundImg);
     Engine.update(engine);
     //strokeWeight(4);
@@ -66,9 +70,10 @@ function draw(){
     bird.display();
     platform.display();
     //log6.display();
-    slingshot.display();    
+    slingshot.display(); 
+       
 }
-
+}
 function mouseDragged(){
     if (gameState!=="launched"){
         Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
@@ -85,4 +90,20 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+ async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata")
+    var responseJSON = await response.json()
+    console.log (responseJSON)
+    var datetime = responseJSON.datetime
+    var hour = datetime.slice (11,13); 
+    if (hour>=06 && hour<=19) {
+
+    
+    bg = "sprites/bg.png"
+    }
+    else {
+    bg = "sprites/bg2.jpg"
+    }
+    backgroundImg = loadImage (bg);
 }
